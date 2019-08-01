d2b264a6134f556dcbe662e13d800912c6bced85
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2019-08-01 13:14:53 +0200

    Update EpigenScores.R

--- EpigenScores.R
+++ EpigenScores.R
@@ -2,8 +2,6 @@ library(rtracklayer)
 library(data.table)
 duljinekromosoma <- readRDS("duljinekromosoma.RDS")
 
-#test_bw <- file.path("D75_bixH3K36me3_sequence.bw")
-
 MakeAnEpigenScoreTable <- function(test_bw){
   gr <- import(test_bw)
   gr<-gr[seqnames(gr)%in%names(duljinekromosoma)]
@@ -11,8 +9,10 @@ MakeAnEpigenScoreTable <- function(test_bw){
   ddt[,region:=start%/%1000]
   ddt[,region_id:=paste(seqnames,region,sep="_")]
   ddt[,Score:=sum(score*width)/sum(width),by=region_id]
-  ddt[,.N, .(region_id,Score)][,.(region_id,Score)]
+  ddt[,Epigen:=str_extract(test_bw, ".*(?=(_sequence))")]
+  ddt[,.N, .(Epigen,region_id,Score)][,.(Epigen,region_id,Score)]
 }
-#bixH3K36me3 <- MakeAnEpigenScoreTable(test_bw)
 
 allEpigenomeTables<-lapply(list.files(pattern=".bw"), function(x)MakeAnEpigenScoreTable(x))
+names(allEpigenomeTables) <- str_extract(list.files(pattern=".bw"), ".*(?=(_sequence))")
+                           
