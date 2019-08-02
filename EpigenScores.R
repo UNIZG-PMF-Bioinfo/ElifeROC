824ad110e8d79de5d9168da8ee61fe5c8a0332c3
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2019-08-02 09:09:52 +0200

    Update EpigenScores.R

--- EpigenScores.R
+++ EpigenScores.R
@@ -2,6 +2,8 @@ library(rtracklayer)
 library(data.table)
 duljinekromosoma <- readRDS("duljinekromosoma.RDS")
 
+# Preparing the table with values of all epigenetic marks:
+# Marks are saved in files named Samplename_Epigenmark_sequence.bw
 MakeAnEpigenScoreTable <- function(test_bw){
   gr <- import(test_bw)
   gr<-gr[seqnames(gr)%in%names(duljinekromosoma)]
@@ -15,4 +17,18 @@ MakeAnEpigenScoreTable <- function(test_bw){
 
 allEpigenomeTables<-lapply(list.files(pattern=".bw"), function(x)MakeAnEpigenScoreTable(x))
 names(allEpigenomeTables) <- str_extract(list.files(pattern=".bw"), ".*(?=(_sequence))")
-                           
+allEpigenomeTables <- do.call("rbind",allEpigenomeTables)
+Epigens<- dcast(allEpigenomeTables, region_id ~ Epigen , value.var="Score", fill=0)
+saveRDS(Epigens, "allEpigenomeTables.RDS")
+
+#Preparing the Integration sites table:
+bix<-fread("BIXhg38.bed")
+bix[,bin:=V2%/%1000]
+bix[,region:=paste(V1,bin, sep="_")]
+colnames(bix)[7] <- "region_id"
+
+setkey(Epigens,region_id)
+setkey(bix,region_id)
+
+bixepi<-merge(bix,epi, all.x=T)
+saveRDS(bixepi, file="BIX_hg38_withEpigenValues.RDS")
