78e7cfa64c01e56afe930cc5fa258b4565117295
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2019-08-02 17:45:34 +0200

    Update 1_PreparingIS_and_EpigenTables.R

--- 1_PreparingIS_and_EpigenTables.R
+++ 1_PreparingIS_and_EpigenTables.R
@@ -107,14 +107,16 @@ saveRDS(Epigens, "genomeTilesWithAddedEpigenScores.RDS")
 set.seed(5555)
 BIXmatched <- replicate(10,Epigens[, .SD[sample(.N, takeThisManyBIX, replace = T)], dist][order(-dist)])
 CONTROLmatched <- replicate(10,Epigens[, .SD[sample(.N, takeThisManyCONTROL, replace = T)], dist][order(-dist)])
+BIXNOTmatched <- replicate(10,Epigens[, .SD[sample(.N, 635, replace = T)], dist][order(-dist)])
+CONTROLNOTmatched <- replicate(10,Epigens[, .SD[sample(.N, 449, replace = T)], dist][order(-dist)])
 
 
 doItForCONTROLEpigen <- function(epiname){
-  mean(do.call("cbind",CONTROLmatched[epiname,])>unlist(bixepisplit[["CONTROL"]][order(-dist)][,..epiname]))
+  mean(do.call("cbind",CONTROLmatched[epiname,])<=unlist(bixepisplit[["CONTROL"]][order(-dist)][,..epiname]))
 }
 
 doItForBIXEpigen <- function(epiname){
-  mean(do.call("cbind",BIXmatched[epiname,])>unlist(bixepisplit[["BIX"]][order(-dist)][,..epiname]))
+  mean(do.call("cbind",BIXmatched[epiname,])<=unlist(bixepisplit[["BIX"]][order(-dist)][,..epiname]))
 }
 epinames <- c("D75_H3K36me3","D75_Input","D75_bixH3K36me3","D75_bixInput","D86_H3K36me3", "D86_Input","D86_bixH3K36me3","D86_bixInput","D75_H3K9me2","D75_Ledgf","D75_bixH3K9me2","D75_bixledgf","D86_H3K9me2","D86_Ledgf","D86_bixH3K9me2","D86_bixledgf" )
 CTRL<-sapply(epinames,doItForCONTROLEpigen)
@@ -123,14 +125,12 @@ x <- data.table(CTRL,BIX)
 #####################################
 
 # WHEN NOT MATCHED BY DIST TO NEAREST GENE:
-BIXNOTmatched <- replicate(10,Epigens[, .SD[sample(.N, 635, replace = T)], dist][order(-dist)])
-CONTROLNOTmatched <- replicate(10,Epigens[, .SD[sample(.N, 449, replace = T)], dist][order(-dist)])
 UNMAdoItForCONTROLEpigen <- function(epiname){
-  mean(do.call("cbind",CONTROLNOTmatched[epiname,])>unlist(bixepisplit[["CONTROL"]][order(-dist)][,..epiname]))
+  mean(do.call("cbind",CONTROLNOTmatched[epiname,])<=unlist(bixepisplit[["CONTROL"]][order(-dist)][,..epiname]))
 }
 
 UNMAdoItForBIXEpigen <- function(epiname){
-  mean(do.call("cbind",BIXNOTmatched[epiname,])>unlist(bixepisplit[["BIX"]][order(-dist)][,..epiname]))
+  mean(do.call("cbind",BIXNOTmatched[epiname,])<=unlist(bixepisplit[["BIX"]][order(-dist)][,..epiname]))
 }
 epinames <- c("D75_H3K36me3","D75_Input","D75_bixH3K36me3","D75_bixInput","D86_H3K36me3", "D86_Input","D86_bixH3K36me3","D86_bixInput","D75_H3K9me2","D75_Ledgf","D75_bixH3K9me2","D75_bixledgf","D86_H3K9me2","D86_Ledgf","D86_bixH3K9me2","D86_bixledgf" )
 UNMACTRL<-sapply(epinames,UNMAdoItForCONTROLEpigen)
